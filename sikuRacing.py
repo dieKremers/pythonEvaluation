186efe1762e68ed73016b4bdd081167238132f7a
Michael Kremer <slartibartfast.fjord@t-online.de>
2017-08-13 14:06:09 +0000

    Proto 1 done. Just start sikuRacing.py

--- sikuRacing.py
+++ sikuRacing.py
@@ -1,10 +1,7 @@
-# import the necessary packages
-##from __future__ import print_function
-##from picamera.array import PiRGBArray
-##from picamera import PiCamera
-from soundplayer import Soundplayer
+##from soundplayer import Soundplayer
 from camera_threaded import MyPiVideoStream
 import RPi.GPIO as GPIO
+import pygame
 ##import argparse
 ##import imutils
 import os
@@ -18,23 +15,39 @@ isqualifying = False
 startTime = 0
 finishTime = 0
 vs = MyPiVideoStream((608, 208), 90)
-soundplayer = Soundplayer
 RECEIVER_PIN = 4
 basePath = "/home/pi/projects/pythonEvaluation/images/"
 startTrigger = "/home/pi/projects/pythonEvaluation/images/start_race.txt"
 qualifyingTrigger = "/home/pi/projects/pythonEvaluation/images/start_qualifying.txt"
 finishPicture = "finished"
- 
+
+def play_startRace():
+    pygame.mixer.init()
+    pygame.mixer.music.load("./sounds/beep-02.wav")
+    for j in range(0, 5):
+        pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy() == True:
+            continue
+        time.sleep(1)
+    pygame.mixer.music.load("./sounds/beep-01a.wav")
+    createStartPicture()
+    pygame.mixer.music.play()
+##    while pygame.mixer.music.get_busy() == True:
+##        continue
+        
+def play_RaceOver():
+    pygame.mixer.music.load("./sounds/beep-09.wav")
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy() == True:
+        continue
+
 def callback_func_covered(channel):
     if GPIO.input(channel):
-        ##time.sleep(0.025)
         global capture
-        ##print ("Rising Edge Detected at ", time.time() )
         capture = True
     else:
         time.sleep(0.05)
         capture = False
-        ##print ("Falling Edge Detected at ", str(time.time()) )
 
 def qualifyingCallback(channel):
     if GPIO.input(channel):
@@ -42,20 +55,21 @@ def qualifyingCallback(channel):
         global isqualifying
         global soundplayer
         global finishTime
-        if ((time.time() - startTime)>2.5):
+        if ((time.time() - startTime)>4):
             isqualifying = False
             finishTime = time.time()
 
 def createStartPicture():
-    path = basePath + "startTime_" + str(time.time()) + ".png"
+    global startTime
+    startTime = time.time()
+    path = basePath + "startTime_" + str(startTime) + ".png"
     os.mknod(path)
 
 def run():
 ##    vs.start()
-    soundplayer.play_startRace()
-    createStartPicture()
+    play_startRace()
     # loop over some frames...this time using the threaded stream
-    while os.path.isfile(startTrigger): ##deadline > time.time():
+    while True: 
             # grab the frame from the threaded video stream
             frame = vs.read()
             frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
@@ -64,8 +78,10 @@ def run():
                 timeString = "%.3f" % (picTime)
                 path = basePath + "pic_" + timeString + ".png"
                 cv2.imwrite( path, frame )
+            if not os.path.isfile(startTrigger):
+                break
 
-    soundplayer.play_RaceOver()
+    play_RaceOver()
 
 #-------- Main Program -----------
 GPIO.setmode(GPIO.BCM)
@@ -89,13 +105,11 @@ while True:
         print ("Start Qualifying...")
         os.remove( qualifyingTrigger )
         GPIO.add_event_detect(RECEIVER_PIN, GPIO.RISING, callback=qualifyingCallback)
-        soundplayer.play_startRace()
-        startTime = time.time()
-        createStartPicture()
+        play_startRace()
         isqualifying = True
         while isqualifying:
             time.sleep(0.1)
-        soundplayer.play_RaceOver()
+        play_RaceOver()
         path = basePath + finishPicture + "_" + str(finishTime) + ".png"
         os.mknod(path)
         GPIO.remove_event_detect(RECEIVER_PIN)
